c2c28feb4165f0495480302a20eaf3747013b4ff
TomozQ <inf.4clover@gmail.com>
2021-10-10 23:02:42 +0900

    initial commit2

---
+++ frontend/pages/biahbiah.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function biahbiah() {
+    return (
+        <div>
+            <Image src="/assets/default_user.jpg" width={1366} height={768}/>
+        </div>
+    )
+}
